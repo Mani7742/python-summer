b2edc39b4f4c7d8d1ba411571cb81829b43aab4a
Muhammad Abdul Rehman Shakeel <96472684+Mani7742@users.noreply.github.com>
2024-06-23 08:14:32 +0500

    Add files via upload

---
+++ bitwiseoperatorDay2.py
@@ -0,0 +1,10 @@
+a = 10
+b = 8
+
+# bin function is used to get the binary 
+print(bin(a))
+print(bin(b))
+
+print(a&b,bin(a&b)) # & operator ------> AND
+print(a|b,bin(a|b)) # | operator ------> OR
+print(a^b,bin(a^b)) # ^ operator ------> XOR
