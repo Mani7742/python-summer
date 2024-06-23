2401d7fb0f4a46b4c5ab4df6ce8126ebc6cababe
Muhammad Abdul Rehman Shakeel <96472684+Mani7742@users.noreply.github.com>
2024-06-23 17:38:21 +0500

    Add files via upload

---
+++ TypecastingDay2.py
@@ -0,0 +1,24 @@
+
+# -------------------------------------< Without Type Casting >----------------------------------------------
+# this will concatinate the r and i
+r = input("Enter the Value:")
+i = input("Enter the Value:")
+print(r+i)
+
+
+# -------------------------------------< Type Casting >----------------------------------------------
+
+# -------------------------------------< converting into Interger Value >----------------------------
+a = int(input("Enter the Value:"))
+x = int(input("Enter the Value:"))
+print(a+x)
+
+# -------------------------------------< converting into Float Value >----------------------------
+b = float(input("Enter the Value:"))
+y = float(input("Enter the Value:"))
+print(b+y)
+
+# -------------------------------------< converting into Interger or float Value >------------------
+c = eval(input("Enter the Value:"))
+z = eval(input("Enter the Value:"))
+print(c+z)
