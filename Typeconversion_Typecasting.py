2bb3870d025de07dd8014b94ffcfc111e8822e78
Muhammad Abdul Rehman Shakeel <96472684+Mani7742@users.noreply.github.com>
2024-06-30 16:23:37 +0500

    Add files via upload

---
+++ Typeconversion_Typecasting.py
@@ -0,0 +1,18 @@
+#---------------------------------------------< Type Conversion >---------------------------------------
+# Type conversion is done automatically by python interprator
+a = 5
+b = 2.2
+sum = a + b
+print(sum) # 7.2
+#---------------------------------------------< Type Conversion >---------------------------------------
+# Type casting is done manually in phyton
+
+# a = "5"
+# b = 2.2
+# sum = a + b
+# print(sum) # 7.2
+#The above code will give an error because of different datatypes
+a = int("5")
+b = 2.2
+sum = a + b
+print(sum) # 7.2
