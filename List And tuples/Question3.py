dd1b8e3b22ad8736d199754256357bc3c20acb5a
Muhammad Abdul Rehman Shakeel <96472684+Mani7742@users.noreply.github.com>
2024-07-06 13:08:17 +0500

    Add files via upload

---
+++ List And tuples/Question3.py
@@ -0,0 +1,8 @@
+# WAP to count the Number of Students with Grade "A" in the following tuple
+Tuple1 = ("A","B","C","F","A","A","A","B")
+print(Tuple1.count("A"))
+# Now Sort the values in the Ascending order
+list1 = ["A","B","C","F","A","A","A","B",]
+list1.sort()
+print(list1)
+
