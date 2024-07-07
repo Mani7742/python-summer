fbbac322a321d257467f8c5eb985512b8d974119
Muhammad Abdul Rehman Shakeel <96472684+Mani7742@users.noreply.github.com>
2024-07-07 18:32:57 +0500

    Add files via upload

---
+++ Loops/Question5While.py
@@ -0,0 +1,7 @@
+tuple = (1,4,9,16,25,36,49,64,81,100)
+x = 36
+idx = 0
+while idx < len(tuple):
+    if tuple[idx] == x:
+        print("Found at index", idx)
+    idx = idx + 1    
---
+++ Loops/Questuin4While.py
@@ -0,0 +1,7 @@
+list = [1,4,9,16,25,36,49,64,81,100]
+print(type(list))
+idx = 0
+while idx < len(list):
+    print(list[idx])
+    idx += 1
+#traverse---> Means travel 
