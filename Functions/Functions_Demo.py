6f89a2f021fe5e3b41ad5eb52fd86ce90611c6fa
Muhammad Abdul Rehman Shakeel <96472684+Mani7742@users.noreply.github.com>
2024-07-11 18:46:19 +0500

    Add files via upload

---
+++ Functions/Functions_Demo.py
@@ -0,0 +1,6 @@
+a = int(input("Enter the 1st Number:- "))
+b = int(input("Enter the 2nd Number:- "))
+def Calc_Sum(a,b):
+    sum = a + b
+    return sum
+print("The Sum  of Two Numbers are:",Calc_Sum(a,b))
