5151205170fe5756bc82dcc2aa64807bf2309ed7
Muhammad Abdul Rehman Shakeel <96472684+Mani7742@users.noreply.github.com>
2024-07-12 15:55:14 +0500

    Add files via upload

---
+++ Functions/Questuion1.py
@@ -0,0 +1,7 @@
+a = eval(input("Enter a Number:- "))
+b = eval(input("Enter a Number:- "))
+c = eval(input("Enter a Number:- "))
+def Calc_Averge(a,b,c):
+    averge = (a+b+c)/3
+    return averge
+print(Calc_Averge(a,b,c))
