7d04b28c458deb9bc0d9dd760fcbd19b6509be41
Muhammad Abdul Rehman Shakeel <96472684+Mani7742@users.noreply.github.com>
2024-08-06 21:24:07 +0500

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,8 @@
+a = 10
+b = 10
+print(a)
+print(b)
+print(a,b)
+print(id(a),id(b)) #id is a pre-defined function
+# Both a and b have the same address bcz both variables have the same value
+# id function is used to get the address of the variables
